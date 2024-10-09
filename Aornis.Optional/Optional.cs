6e5ed71ef075be051349352575fa32fb3d76485c
Charlotte Thorne <det@home.aornis.net>
2024-10-09 21:51:05 +0100

    Added doc blocks

--- Aornis.Optional/Optional.cs
+++ Aornis.Optional/Optional.cs
@@ -6,12 +6,21 @@ namespace Aornis
 {
     public struct Optional<TValue> : IOptional, IEquatable<Optional<TValue>>
     {
+        /// <summary>
+        /// Represents the 'empty' value for an Optional(TValue) instance
+        /// </summary>
         public static readonly Optional<TValue> Empty = new Optional<TValue>(default(TValue));
 
         private readonly TValue value;
 
+        /// <summary>
+        /// Returns true if this Optional contains a value, otherwise false
+        /// </summary>
         public bool HasValue { get; }
 
+        /// <summary>
+        /// Returns the value held inside this Optional, or throws InvalidOperationException if this Optional does not contain a value
+        /// </summary>
         public TValue Value
         {
             get
@@ -25,12 +34,24 @@ namespace Aornis
             }
         }
 
+        /// <summary>
+        /// Creates an Optional(TValue) with the given TValue value, if value is equal to the default value for the given TValue (null for reference types, 0-arity constructor for value types)
+        /// then this optional will be empty.
+        /// </summary>
+        /// <param name="value">The value to be stored in this optional type</param>
         public Optional(TValue value) : this()
         {
             this.value = value;
             HasValue = !Optional.IsDefault(value);
         }
 
+        /// <summary>
+        /// Passes the value contained within this Optional and returns the Optional value created by the given mapping function.
+        /// If this Optional is empty, the function will NOT be called.
+        /// </summary>
+        /// <typeparam name="TNewValue">The type of the new value returned by the mapping function</typeparam>
+        /// <param name="mapper">The function to map this Optional(TValue) to an Optional(TNewValue)</param>
+        /// <returns>The value returned by mapper, or Empty</returns>
         public Optional<TNewValue> FlatMap<TNewValue>(Func<TValue, Optional<TNewValue>> mapper)
         {
             if (!HasValue)
@@ -41,6 +62,13 @@ namespace Aornis
             return mapper(value);
         }
 
+        /// <summary>
+        /// Passes the value contained within this Optional and returns the Optional value created by the given mapping function.
+        /// If this Optional is empty, the function will NOT be called.
+        /// </summary>
+        /// <typeparam name="TNewValue">The type of the new value returned by the mapping function</typeparam>
+        /// <param name="mapper">The function to map this Optional(TValue) to an Optional(TNewValue)</param>
+        /// <returns>The value returned by mapper, or Empty</returns>
         public async Task<Optional<TNewValue>> FlatMapAsync<TNewValue>(Func<TValue, Task<Optional<TNewValue>>> mapper)
         {
             if (!HasValue)
@@ -51,6 +79,11 @@ namespace Aornis
             return await mapper(value);
         }
 
+        /// <summary>
+        /// Calls the given function only if this Optional DOES contain a value
+        /// </summary>
+        /// <param name="callback">The function to call if this Optional contains a value</param>
+        /// <returns>This Optional</returns>
         public Optional<TValue> IfPresent(Action<TValue> callback)
         {
             if (!HasValue)
@@ -62,6 +95,11 @@ namespace Aornis
             return this;
         }
 
+        /// <summary>
+        /// Calls the given function only if this Optional DOES contain a value
+        /// </summary>
+        /// <param name="callback">The function to call if this Optional contains a value</param>
+        /// <returns>This Optional</returns>
         public async Task<Optional<TValue>> IfPresentAsync(Func<TValue, Task> callback)
         {
             if (!HasValue)
@@ -73,6 +111,11 @@ namespace Aornis
             return this;
         }
 
+        /// <summary>
+        /// Calls the given function only if this Optional does NOT contain a value
+        /// </summary>
+        /// <param name="callback">The function to call if this Optional is empty</param>
+        /// <returns>This Optional</returns>
         public Optional<TValue> IfNotPresent(Action callback)
         {
             if (HasValue)
@@ -84,6 +127,11 @@ namespace Aornis
             return this;
         }
 
+        /// <summary>
+        /// Calls the given function only if this Optional does NOT contain a value
+        /// </summary>
+        /// <param name="callback">The function to call if this Optional is empty</param>
+        /// <returns>This Optional</returns>
         public async Task<Optional<TValue>> IfNotPresentAsync(Func<Task> callback)
         {
             if (HasValue)
@@ -95,6 +143,12 @@ namespace Aornis
             return this;
         }
 
+        /// <summary>
+        /// If this optional contains a value, calls the given mapping function and passes the value inside this Optional
+        /// </summary>
+        /// <typeparam name="TNewValue">The type of the value created by the mapping function</typeparam>
+        /// <param name="mapper">The mapping function to map this Optional(TValue) into an Optional(TNewValue)</param>
+        /// <returns>The newly mapped value, or Empty if this Optional is empty</returns>
         public Optional<TNewValue> Map<TNewValue>(Func<TValue, TNewValue> mapper)
         {
             if (!HasValue)
@@ -105,6 +159,12 @@ namespace Aornis
             return Optional.Of(mapper(value));
         }
 
+        /// <summary>
+        /// If this optional contains a value, calls the given mapping function and passes the value inside this Optional
+        /// </summary>
+        /// <typeparam name="TNewValue">The type of the value created by the mapping function</typeparam>
+        /// <param name="mapper">The mapping function to map this Optional(TValue) into an Optional(TNewValue)</param>
+        /// <returns>The newly mapped value, or Empty if this Optional is empty</returns>
         public async Task<Optional<TNewValue>> MapAsync<TNewValue>(Func<TValue, Task<TNewValue>> mapper)
         {
             if (!HasValue)
@@ -115,36 +175,31 @@ namespace Aornis
             return Optional.Of(await mapper(value));
         }
 
-        public Optional<TValue> OrElse(TValue fallback)
+        /// <summary>
+        /// If this optional is empty, returns the given TValue. Otherwise returns the value contained inside this Optional.
+        /// </summary>
+        /// <param name="fallback">Fallback value if this Optional is empty</param>
+        /// <returns>this.Value if this Optional is not empty, otherwise the given fallback value</returns>
+        public TValue OrElse(TValue fallback)
         {
-            if (HasValue)
-            {
-                return this;
-            }
-
-            return Optional.Of(fallback);
+            return HasValue ? this.Value : fallback;
         }
 
-        public Optional<TValue> OrElse(Func<TValue> func)
+        /// <summary>
+        /// If this optional is empty, returns the value created by the given function. Otherwise returns the value contained inside this Optional.
+        /// </summary>
+        /// <param name="fallback">Fallback value if this Optional is empty</param>
+        /// <returns>this.Value if this Optional is not empty, otherwise the given fallback value</returns>
+        public TValue OrElse(Func<TValue> func)
         {
-            if (HasValue)
-            {
-                return this;
-            }
-
-            return Optional.Of(func());
+            return HasValue ? this.Value : func();
         }
 
-        public async Task<Optional<TValue>> OrElseAsync(Func<Task<TValue>> func)
-        {
-            if (HasValue)
-            {
-                return this;
-            }
-
-            return Optional.Of(await func());
-        }
-
+        /// <summary>
+        /// If this optional is empty, returns the value created by the given function. Otherwise returns the value contained inside this Optional.
+        /// </summary>
+        /// <param name="fallback">Fallback value if this Optional is empty</param>
+        /// <returns>this.Value if this Optional is not empty, otherwise the given fallback value</returns>
         public Optional<TValue> OrElse(Func<Optional<TValue>> func)
         {
             if (HasValue)
@@ -155,6 +210,26 @@ namespace Aornis
             return func();
         }
 
+        /// <summary>
+        /// If this optional is empty, returns the value created by the given function. Otherwise returns the value contained inside this Optional.
+        /// </summary>
+        /// <param name="fallback">Fallback value if this Optional is empty</param>
+        /// <returns>this.Value if this Optional is not empty, otherwise the given fallback value</returns>
+        public async Task<Optional<TValue>> OrElseAsync(Func<Task<TValue>> func)
+        {
+            if (HasValue)
+            {
+                return this;
+            }
+
+            return Optional.Of(await func());
+        }
+
+        /// <summary>
+        /// If this optional is empty, returns the value created by the given function. Otherwise returns the value contained inside this Optional.
+        /// </summary>
+        /// <param name="fallback">Fallback value if this Optional is empty</param>
+        /// <returns>this.Value if this Optional is not empty, otherwise the given fallback value</returns>
         public async Task<Optional<TValue>> OrElseAsync(Func<Task<Optional<TValue>>> func)
         {
             if (HasValue)
@@ -164,6 +239,26 @@ namespace Aornis
 
             return await func();
         }
+
+        /// <summary>
+        /// Converts the given TValue into an Optional(TValue)
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator Optional<TValue>(TValue value)
+        {
+            return Optional.Of(value);
+        }
+
+        /// <summary>
+        /// Implicit operator to convert Optional.Empty into Optional(TValue).Empty
+        /// </summary>
+        /// <param name="value">The nongeneric Optional to convert</param>
+        public static implicit operator Optional<TValue>(Optional value)
+        {
+            return Empty;
+        }
+
+        #region Equality Operators
 
         public override int GetHashCode()
         {
@@ -198,18 +293,6 @@ namespace Aornis
         public static bool operator ==(Optional lhs, Optional<TValue> rhs) => !rhs.HasValue;
         public static bool operator !=(Optional lhs, Optional<TValue> rhs) => !(lhs == rhs);
 
-        public static implicit operator Optional<TValue>(TValue value)
-        {
-            return Optional.Of(value);
-        }
-
-        /// <summary>
-        /// Implicit operator to convert Optional.Empty into Optional(TValue).Empty
-        /// </summary>
-        /// <param name="value">The nongeneric Optional to convert</param>
-        public static implicit operator Optional<TValue>(Optional value)
-        {
-            return Empty;
-        }
+        #endregion
     }
 }
